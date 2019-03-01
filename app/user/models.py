9951ac9c7fbb0dd327c73bd93341917113f28767
Ken <kenofuniverse@gmail.com>
2019-03-01 17:33:09 +0800

    add user model and serializer

--- app/user/models.py
+++ app/user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+class User(AbstractBaseUser):
+  # User username
+  username = models.CharField(
+    max_length=256,
+    null=False,
+    unique=True
+  )
+  # User firstname
+  first_name = models.CharField(
+    max_length=256,
+    null=False
+  )
+  # User lastname
+  last_name = models.CharField(
+    max_length=256,
+    null=False
+  )
+
+  USERNAME_FIELD = 'username'
